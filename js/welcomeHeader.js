cd7dd2c34d85ea1bead0be8c678ac3c05c5e9f6a
Saharach Thaiutai <saharachthaiutai@outlook.com>
2024-11-06 14:53:33 +0000

    Updates 6/11

---
+++ js/welcomeHeader.js
@@ -0,0 +1,5 @@
+'use strict'
+
+function hello() {
+    alert('Hello, world!')
+}
---
+++ welcomeHeader.js
@@ -0,0 +1,6 @@
+'use strict'
+
+document.documentElement.classList.add('preparation');
+window.onload = function() {
+    document.documentElement.classList.remove('preparation');
+};
